bed92d3bc295c2273adf18a00fa59e9412d1091e
zahrakhan <zahrakhadijha@gmail.com>
2020-07-16 16:10:18 -0400

    create routes in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./screens/Home/Home";
+import About from "./screens/About/About";
+import AccountInfo from "./screens/AccountInfo/AccountInfo";
+import EditAccount from "./screens/EditAccount/EditAccount";
+import DeleteAccount from "./screens/DeleteAccount/DeleteAccount";
+import SignIn from "./screens/SignIn/SignIn";
+import SignUp from "./screens/SignUp/SignUp";
+import Logger from "./screens/Logger/Logger";
+import Progress from "./screens/Progress/Progress";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/account-signup" component={SignUp} />
+        <Route exact path="/account-signin" component={SignIn} />
+        <Route exact path="/account-info" component={AccountInfo} />
+        <Route exact path="/edit-account" component={EditAccount} />
+        <Route exact path="/delete-account" component={DeleteAccount} />
+        <Route exact path="/logger" component={Logger} />
+        <Route exact path="/progress" component={Progress} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
