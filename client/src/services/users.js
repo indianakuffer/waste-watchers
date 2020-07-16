e059f3759217b6870522b7ec340044c181fae6f9
Indiana Kuffer <indianakuffer@gmail.com>
2020-07-16 16:25:26 -0400

    Update users.js
    added missing return in getUsers()

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -3,6 +3,7 @@ import api from './apiConfig'
 export const getUsers = async () => {
   try {
     const response = await api.get('/users')
+    return response.data
   } catch (error) {
     throw error
   }
