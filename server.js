f351e1a79342ea459dc038b363155a5018319892
zahrakhan <zahrakhadijha@gmail.com>
2020-07-16 13:52:54 -0400

    create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const userRoutes = require("./routes/users-routes");
+const db = require("./db/connection");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(logger("dev"));
+app.use("api", userRoutes);
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
