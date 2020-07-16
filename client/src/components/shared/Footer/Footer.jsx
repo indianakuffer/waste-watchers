74d409b209aaade806bbbf9f9c38c339eac629e3
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-07-16 18:21:44 -0400

    added Footer Support and Company links

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
+    <Footer>
+      <Footer.Wrapper>
+        <Footer.Row>
+          <Footer.Column>
+            <Footer.Title>Support</Footer.Title>
+            <Footer.Link href="#">Help Center</Footer.Link>
+            <Footer.Link href="#">Contact Us</Footer.Link>
+            <Footer.Link href="#">Cookies</Footer.Link>
+            <Footer.Link href="#">Privacy & Terms</Footer.Link>
+            <Footer.Link href="#">Sitemap</Footer.Link>
+          </Footer.Column>
+          <Footer.Column>
+            <Footer.Title>Company</Footer.Title>
+            <Footer.Link href="#">About Us</Footer.Link>
+            <Footer.Link href="#">Jobs</Footer.Link>
+            <Footer.Link href="#">Press</Footer.Link>
+            <Footer.Link href="#">Investor Relations</Footer.Link>
+          </Footer.Column>
 
-    </div>
+        </Footer.Row>
+      </Footer.Wrapper>
+    </Footer>
   )
 }
