782b094a16349ea51beff730fc6dfdec0ec067a5
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-07-17 12:04:30 -0400

    css test

--- client/src/components/shared/Input/Input.jsx
+++ client/src/components/shared/Input/Input.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledInput = styled.input`
-  //css here
+width: 309px;
+  height: 45px;
+  border-radius: 5px;
+  background-color: #dadada;
 `
 
 export default function Input(props) {
