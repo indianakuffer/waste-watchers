35f4c58f92855dae13eb25c4203050d901ca5d2b
Indiana Kuffer <indianakuffer@gmail.com>
2020-07-16 22:58:06 -0400

    add Button shared component

--- client/src/components/shared/Button/Button.jsx
+++ client/src/components/shared/Button/Button.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-export default function Button() {
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`
+
+const StyledButton = styled.button`
+  background: ${props => props.color ? props.color : 'grey'};
+  height: 45px;
+  border: none;
+  border-radius: 5px;
+  padding: 10px;
+  box-sizing: border-box;
+  color: white;
+  font-family: 'Nunito', arial, sans-serif;
+  font-size: 24px;
+  font-weight: bold;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+`
+
+export default function Button(props) {
   return (
-    <div>
+    <>
+      {props.buttonLink ?
+        <StyledLink to={props.buttonLink}>
+          <StyledButton color={props.color}>{props.buttonText}</StyledButton>
+        </StyledLink>
+        :
+        <StyledButton color={props.color}>{props.buttonText}</StyledButton>
+      }
 
-    </div>
+    </>
   )
 }
