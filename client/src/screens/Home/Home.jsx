ac8b1139a43b8d155a7165fc7b2d07dd6c19145e
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-07-17 11:12:33 -0400

    fixed pictures

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <Layout>
       <Jumbotron
-        imageUrl='https://ibb.co/RD0Lps1'
+        imageUrl='https://i.imgur.com/aat01sL.png'
         color='#ffce51'
         bigText='Waste Watchers is a global recycling collective.'
         smallText='Learn more about what we do and the values driving our organization.'
@@ -15,7 +15,7 @@ export default function Home() {
         buttonLink='/about'
       />
       <Jumbotron
-        imageUrl='https://ibb.co/R75Pn2W'
+        imageUrl='https://i.imgur.com/o7mzJEe.png'
         color='#31c96e'
         bigText='A green Earth is a happy Earth, and a happy Earth makes for happy people.'
         smallText='Join us and turn your recyclables into trees!'
@@ -23,7 +23,7 @@ export default function Home() {
         buttonLink='/signup'
       />
       <Jumbotron
-        imageUrl='https://ibb.co/ZWnP5W0'
+        imageUrl='https://i.imgur.com/f1ytCY7.png'
         color='#235ae5'
         bigText='The first step of your recycling journey starts here.'
         smallText='All the recycling resources you need.'
@@ -31,7 +31,7 @@ export default function Home() {
         buttonLink='/signup'
       />
       <Jumbotron
-        imageUrl='https://ibb.co/zhSHLX3'
+        imageUrl='https://i.imgur.com/b2PQ8oB.png'
         color='#ff7373'
         quote='-Brian A., NYC'
         bigText={`"I feel bad for not participating in something so well designed."`}
