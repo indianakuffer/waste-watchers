38b1e6c19d7770a139fe30924ec16d2c80b2098b
zahrakhan <zahrakhadijha@gmail.com>
2020-07-17 11:35:28 -0400

    CSS done

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -12,7 +12,7 @@ const NavBar = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: ;
+  padding: 0px 100px;
 `;
 
 const Links = styled(NavLink)`
@@ -20,36 +20,65 @@ const Links = styled(NavLink)`
   text-decoration: none;
   font-family: Nunito;
   color: #000000;
-  // background-color: red;
-  font-size: 17px;
+  font-size: 15px;
   height: 23px;
-  width: 110px;
   margin: 10px;
+  text-align: center;
 `;
 
 const LinksContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
-  margin: 20px;
+  // justify-content: space-evenly;
+  // margin: 30px;
 `;
 
-const LogoContainer = styled.div``;
+const Logo = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: #000000;
+  font-size: 20px;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 const AccountButton = styled.button`
   border-radius: 50%;
   height: 35px;
   width: 35px;
   border-style: none;
+  background-image: url("https://i.imgur.com/SUHhp7V.png");
+  background-position: center;
+  margin-left: 10px;
+`;
+
+const Waste = styled.span`
+  font-weight: 700;
+`;
+
+const Watchers = styled.span`
+  font-weight: 200;
+  position: relative;
+  top: 18px;
+  right: 25px;
+`;
+
+const LogoImage = styled.img`
+  height: 60px;
 `;
 
 export default function Nav() {
   return (
     <NavBar>
       <LogoContainer>
-        <NavLink classname="logo" to="/">
-          Waste Wasters
-        </NavLink>
+        <Logo classname="logo" to="/">
+          <LogoImage src="https://i.imgur.com/lp2jwK3.png" />
+          <Waste>Waste</Waste> <Watchers>Watchers</Watchers>
+        </Logo>
       </LogoContainer>
       <LinksContainer>
         <Links className="log-recycling" to="/log">
@@ -61,7 +90,9 @@ export default function Nav() {
         <Links className="link" to="/about">
           About Us
         </Links>
-        <AccountButton></AccountButton>
+        <AccountButton>
+          {/* <ButtonIcon src="https://i.imgur.com/SUHhp7V.png" /> */}
+        </AccountButton>
       </LinksContainer>
     </NavBar>
   );
