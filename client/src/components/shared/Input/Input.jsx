7ca2a4383f7ee0cd5bbaef36741c46d72bd64190
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-07-17 16:55:10 -0400

    input

--- client/src/components/shared/Input/Input.jsx
+++ client/src/components/shared/Input/Input.jsx
@@ -6,10 +6,29 @@ width: 309px;
   height: 45px;
   border-radius: 5px;
   background-color: #dadada;
+  border: none;
+  position: relative;
+  //----- text -----// 
+  font-family: Nunito;
+  font-size: 17px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #838383;
+`
+const StyledImage = styled.img`
+position: absolute;
+top: 5px;
 `
 
 export default function Input(props) {
+  console.log(props.image)
   return (
-    <StyledInput placeholder={props.placeholder}></StyledInput>
+    <>
+      <StyledInput placeholder={props.placeholder} type={props.type}></StyledInput>
+      <StyledImage src={props.image} />
+    </>
   )
 }
--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import Layout from '../../components/shared/Layout/Layout'
 import Input from '../../components/shared/Input/Input'
 
+
 export default function SignUp(props) {
   return (
     <Layout loggedIn={props.loggedIn}>
       <Input
-        placeholder=''
+        placeholder='Name'
+        image="../"
+      />
+      <Input
+        placeholder='Email'
+        image={require("../../images/input-icons/envelope.svg")}
+      />
+      <Input type="password"
+        placeholder='Password'
         image=''
       />
     </Layout>
