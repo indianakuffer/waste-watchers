189681f9d135de5561b0c69b1669744d961fb5c1
Indiana Kuffer <indianakuffer@gmail.com>
2020-07-16 22:29:59 -0400

    build Jumbotron component, examples on Home

--- client/src/components/shared/Jumbotron/Jumbotron.jsx
+++ client/src/components/shared/Jumbotron/Jumbotron.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-export default function Jumbotron() {
+const JumboContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 800px;
+  background-image: url(${props => props.imageUrl});
+  background-size: cover;
+  * {
+    box-sizing: border-box;
+  }
+`
+const TextBox = styled.div`
+  display: flex;
+  flex-flow: column; 
+  justify-content: center;
+  align-items: center;
+  font-family: 'Nunito', arial, sans-serif;
+  padding: 100px 0px;
+  color: white;
+  text-align: center;
+  background-color: ${props => props.color ? props.color + 'BF' : 'white'};
+  width: 83%;
+  min-height: 480px;
+`
+const BigText = styled.div`
+  font-weight: bold;
+  font-size: 40px;
+  width: 90%;
+  max-width: 900px;
+  margin-bottom: ${props => props.quote ? '10px' : '20px'};
+`
+const Quoted = styled.div`
+  font-size: 25px;
+  margin-bottom: 45px;
+`
+const SmallText = styled.div`
+  font-style: italic;
+  font-size: 30px;
+  max-width: 470px;
+  margin-bottom: 45px;
+`
+const JumboButton = styled.button`
+  color: white;
+  background: #aeaeae;
+  border: none;
+  width: 236px;
+  height: 72px;
+  font-size: 17px;
+  font-weight: bold;
+`
+
+export default function Jumbotron(props) {
   return (
-    <div>
-
-    </div>
+    <JumboContainer imageUrl={props.imageUrl}>
+      <TextBox color={props.color}>
+        <BigText quote={props.quote}>{props.bigText}</BigText>
+        {props.quote && <Quoted>{props.quote}</Quoted>}
+        <SmallText>{props.smallText}</SmallText>
+        <Link to={props.buttonLink}>
+          <JumboButton>{props.buttonText}</JumboButton>
+        </Link>
+      </TextBox>
+    </JumboContainer>
   )
 }
