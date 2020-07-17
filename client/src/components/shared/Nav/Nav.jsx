7f349920ab5bd81c6be5632e945c380e9da6925a
zahrakhan <zahrakhadijha@gmail.com>
2020-07-16 20:45:41 -0400

    create basic nav boilerplate

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 export default function Nav() {
   return (
-    <div>
-
-    </div>
-  )
+    <nav>
+      <div className="nav">
+        <NavLink classname="logo" to="/">
+          Waste Wasters
+        </NavLink>
+        <div className="links">
+          <NavLink className="link" to="/log">
+            Log Recycling
+          </NavLink>
+          <NavLink className="link" to="/progress">
+            My Progress
+          </NavLink>
+          <NavLink className="link" to="/about">
+            About Us
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
 }
