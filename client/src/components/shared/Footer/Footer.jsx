58b366c820fa2f42a2118ef0efa6bfc0a5bd492c
Indiana Kuffer <indianakuffer@gmail.com>
2020-07-17 10:30:07 -0400

    add Footer.jsx

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -1,28 +1,62 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: space-around;
+  background-color: #dadada;
+  height: 261px;
+  padding-top: 40px;
+`
+const FooterCategory = styled.div`
+  display: flex;
+  flex-flow: column;
+  .title {
+    font-size: 22px;
+    font-weight: bold;
+  }
+  a {
+    color: black;
+    text-decoration: none;
+    font-size: 17px;
+    font-weight: 300;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`
+const Logo = styled.div`
+`
 
 export default function Footer() {
   return (
-    <Footer>
-      <Footer.Wrapper>
-        <Footer.Row>
-          <Footer.Column>
-            <Footer.Title>Support</Footer.Title>
-            <Footer.Link href="#">Help Center</Footer.Link>
-            <Footer.Link href="#">Contact Us</Footer.Link>
-            <Footer.Link href="#">Cookies</Footer.Link>
-            <Footer.Link href="#">Privacy & Terms</Footer.Link>
-            <Footer.Link href="#">Sitemap</Footer.Link>
-          </Footer.Column>
-          <Footer.Column>
-            <Footer.Title>Company</Footer.Title>
-            <Footer.Link href="#">About Us</Footer.Link>
-            <Footer.Link href="#">Jobs</Footer.Link>
-            <Footer.Link href="#">Press</Footer.Link>
-            <Footer.Link href="#">Investor Relations</Footer.Link>
-          </Footer.Column>
-
-        </Footer.Row>
-      </Footer.Wrapper>
-    </Footer>
+    <FooterContainer>
+      <Logo>
+        <img />
+      </Logo>
+      <FooterCategory>
+        <div className='title'>Connect with Us</div>
+        <a href='#' target='_blank' rel='noopener noreferrer'><img /></a>
+        <a href='#' target='_blank' rel='noopener noreferrer'><img /></a>
+        <a href='#' target='_blank' rel='noopener noreferrer'><img /></a>
+        <a href='#' target='_blank' rel='noopener noreferrer'><img /></a>
+      </FooterCategory>
+      <FooterCategory>
+        <div className='title'>Support</div>
+        <Link to="/">Help Center</Link>
+        <Link to="/">Contact Us</Link>
+        <Link to="/">Cookies</Link>
+        <Link to="/">Privacy & Terms</Link>
+        <Link to="/">Sitemap</Link>
+      </FooterCategory>
+      <FooterCategory>
+        <div className='title'>Company</div>
+        <Link to="/">About Us</Link>
+        <Link to="/">Jobs</Link>
+        <Link to="/">Press</Link>
+        <Link to="/">Investor Relations</Link>
+      </FooterCategory>
+    </FooterContainer>
   )
 }
