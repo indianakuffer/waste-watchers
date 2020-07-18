a4838c8a05247120a0ae68c3ee4ae9ad50a7cccf
Indiana Kuffer <indianakuffer@gmail.com>
2020-07-18 12:35:43 -0400

    add delete functionality

--- client/src/screens/AccountInfo/AccountInfo.jsx
+++ client/src/screens/AccountInfo/AccountInfo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../../components/shared/Layout/Layout'
+import { Link } from 'react-router-dom'
 
 export default function AccountInfo(props) {
   return (
     <Layout loggedIn={props.loggedIn}>
-
+      <Link to={`/account/${props.loggedIn}/delete`}>Temp Delete</Link>
     </Layout>
   )
 }
