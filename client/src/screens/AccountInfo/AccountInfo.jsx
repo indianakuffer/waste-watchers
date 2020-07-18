64ac75d002de9e1cc15c912af31ed4ff2b48a5df
zahrakhan <zahra.khan1@icloud.com>
2020-07-18 13:24:35 -0400

    build account info page with basic styling

--- client/src/screens/AccountInfo/AccountInfo.jsx
+++ client/src/screens/AccountInfo/AccountInfo.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
-import Layout from '../../components/shared/Layout/Layout'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
+import styled from "styled-components";
+import Button from "../../components/shared/Button/Button";
+
+const ScreenContainer = styled.div`
+  position: relative;
+  flex: 1;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+
+  .squiggle {
+    position: absolute;
+    width: 100%;
+    height: 50%;
+    z-index: -10;
+    background: url('https://i.imgur.com/JqlYCOL.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+
+`
+
+const AccountDiv = styled.div`
+display: flex;
+flex-flow: column;
+align-items: center;
+
+button {
+  margin: 10px 0;
+  font-size: 18px;
+}
+
+`
+
+const TopImage = styled.div`
+  top: 0;
+`
+
+const BottomImage = styled.div`
+  bottom: 0;
+  transform: scaleY(-1) scaleX(-1);
+`
+
 
 export default function AccountInfo(props) {
   return (
     <Layout loggedIn={props.loggedIn}>
-      <Link to={`/account/${props.loggedIn}/delete`}>Temp Delete</Link>
+      <ScreenContainer>
+        <TopImage className="squiggle" />
+        <AccountDiv>
+        <h1>Manage My Account</h1>
+        <h3>Welcome, Dakota!</h3>
+      <Button
+        color="#5a83ec"
+        buttonText="Edit Account Information" />
+      <Button
+        color="#aeaeae"
+        buttonText="Settings"
+        buttonLink="#" />
+
+      <Button
+        color="#ff7373"
+        buttonText="Delete My Account"
+            buttonLink="/" />
+          </AccountDiv>
+        <BottomImage className="squiggle" />
+        </ScreenContainer>
     </Layout>
-  )
+  );
 }
