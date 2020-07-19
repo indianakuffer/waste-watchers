9a3167970853ab8a209e0dd4d597e603cc9497dc
Indiana Kuffer <indianakuffer@gmail.com>
2020-07-19 11:53:24 -0400

    functionality progress

--- client/src/screens/AccountInfo/AccountInfo.jsx
+++ client/src/screens/AccountInfo/AccountInfo.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/shared/Layout/Layout";
 import styled from "styled-components";
 import Button from "../../components/shared/Button/Button";
+import { getUser } from '../../services/users'
 
 const ScreenContainer = styled.div`
   position: relative;
@@ -20,8 +21,7 @@ const ScreenContainer = styled.div`
     background-size: contain;
     background-repeat: no-repeat;
   }
-`;
-
+`
 const AccountDiv = styled.div`
   display: flex;
   flex-flow: column;
@@ -33,42 +33,46 @@ const AccountDiv = styled.div`
     font-size: 18px;
     box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
   }
-`;
-
+`
 const UserPhoto = styled.img`
 margin: -10px;
 border-radius: 50%;
 background-color: transparent;
 
 `
-
 const TopImage = styled.div`
   top: 0;
-`;
-
+`
 const BottomImage = styled.div`
   bottom: 0;
   transform: scaleY(-1) scaleX(-1);
-`;
-
-
+`
 
 export default function AccountInfo(props) {
+  let [userData, setUserData] = useState(null)
+
+  useEffect(() => {
+    const helper = async id => {
+      const response = await getUser(id)
+      setUserData(response.accountInfo)
+    }
+    helper(props.loggedIn)
+  }, [])
+
   return (
     <Layout loggedIn={props.loggedIn}>
       <ScreenContainer>
         <TopImage className="squiggle" />
         <AccountDiv>
           <h1>Manage My Account</h1>
-          <UserPhoto src="https://i.imgur.com/UcwbCnR.png" />
-          <h3>
-            Welcome, Dakota!
-            <br />
-            What would you like to do next?{" "}
-          </h3>
-          <Button color="#5a83ec" buttonText="Edit Account Information" />
-          <Button color="#aeaeae" buttonText="Settings" buttonLink="#" />
-
+          {userData &&
+            <>
+              <UserPhoto src={userData.profileImg} />
+              <h3>Welcome, {userData.username}!<br />What would you like to do next?{" "}</h3>
+            </>
+          }
+          <Button color="#5a83ec" buttonText="Edit Account Information" buttonLink={`/account/${props.loggedIn}/edit`} />
+          <Button color="#aeaeae" buttonText="Settings" />
           <Button
             color="#ff7373"
             buttonText="Delete My Account"
