f73948a2d26c5b68e7e0ba115904dfdb28856e98
zahrakhan <zahra.khan1@icloud.com>
2020-07-20 12:24:53 -0400

    built about page containers and styling

--- client/src/screens/About/About.jsx
+++ client/src/screens/About/About.jsx
@@ -1,65 +1,91 @@
-import React from 'react'
-import Layout from '../../components/shared/Layout/Layout'
-import styled from 'styled-components'
+import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
+import styled from "styled-components";
+import Jumbotron from "../../components/shared/Jumbotron/Jumbotron";
 
-const TextContainer = styled.div`
-display: flex;
-  width: 1123px;
-  height: 545px;
-  position: relative; 
-  left: 22px;
-  top: 99px;
-  font-family: Nunito;
+const AboutContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+`;
+
+const AboutHeading = styled.div`
+  font-family: 'Nunito', sans-serif;
+  margin-top: 60px;
   font-size: 60px;
-  font-weight: bold;
-  color: #000000;
-`
-const AboutTitle = styled.div`
-  font-family: Nunito;
-  font-size: 40px;
   text-align: center;
   align-items: center;
   font-weight: bold;
   color: #000000;
-`
-const TextBorder = styled.div`
-{
-  position: relative;
-   left: 18px; 
-   top: 18px;
+
+`;
+
+const HeadingUnderline = styled.div`
+  text-align: center;
   width: 150px;
   height: 5px;
   background-color: #000000;
-}
-`
-const AlignAll = styled.div`
-{
+  margin-top: 20px;
+`;
+
+
+const AboutText = styled.p`
   display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  align-content: center
-}
-}
+  justify-content: center;
+  text-align: center;
+  width: 1123px;
+  height: 545px;
+  font-family: 'Nunito', sans-serif;
+  font-size: 80px;
+  font-weight: bold;
+  color: #000000;
+  margin-top: 60px;
+  margin-bottom: 60px;
+`;
+
+const BlueSquiggle = styled.img`
+width: 100%;
+z-index: -10;
+
 `
+
+const GreenSquiggle = styled.img`
+width: 100%;
+z-index: -10;
+
+`
+
 
 export default function About(props) {
   return (
     <Layout loggedIn={props.loggedIn} setLoggedIn={props.setLoggedIn}>
-
-      <AlignAll>
-        <AboutTitle>
-          Who We Are
-        </AboutTitle>
-        <br></br>
-        <TextBorder />
-        <TextContainer>
-          Waste Watchers is a global
-          recycling collective dedicated
-          to empowering humans
-          everywhere to actively engage
-          in recycling.
-        </TextContainer>
-      </AlignAll>
+      <Jumbotron
+          className="jumbotron"
+          imageUrl="https://i.imgur.com/aat01sL.png"
+          color="#ffce51"
+          bigText="We're reframing recylcing"
+          smallText="About Waste Watchers"
+        />
+      <AboutContainer>
+        <AboutHeading>Who We Are</AboutHeading>
+        <HeadingUnderline />
+        <AboutText>
+          Waste Watchers is a global recycling collective dedicated to
+          empowering humans everywhere to actively engage in recycling.
+        </AboutText>
+        <BlueSquiggle src="https://i.imgur.com/waL4ZJN.png" />
+        <AboutHeading>What We Do</AboutHeading>
+        <HeadingUnderline />
+        <AboutText>
+          We empower our members through providing personalized analytics and tree planting incentives based on identified recyling habits.
+        </AboutText>
+        <GreenSquiggle src="https://i.imgur.com/Xq3HU2x.png"/>
+        <AboutHeading>Why We Do What We Do</AboutHeading>
+        <HeadingUnderline />
+        <AboutText>
+         Life is hard enough already, recycling shouldn't be. We're here to change that and to keep our planet healthy, happy, and green.
+        </AboutText>
+      </AboutContainer>
     </Layout>
-  )
+  );
 }
