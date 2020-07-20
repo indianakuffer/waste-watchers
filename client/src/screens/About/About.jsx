ef949589e83d95cfc16421c859091d2ba4ebdbd8
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-07-20 10:35:40 -0400

    about page

--- client/src/screens/About/About.jsx
+++ client/src/screens/About/About.jsx
@@ -1,33 +1,62 @@
 import React from 'react'
 import Layout from '../../components/shared/Layout/Layout'
-
+import styled from 'styled-components'
 
 const TextContainer = styled.div`
-h1{
+display: flex;
   width: 1123px;
   height: 545px;
+  text-align: center;
+  align-content: center;
   font-family: Nunito;
-  font-size: 80px;
+  font-size: 60px;
   font-weight: bold;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
   color: #000000;
+`
+const AboutTitle = styled.div`
+  display: flex;
+  position: relative;
+  flex-wrap: wrap;
+  justify-content: center;
+  font-family: Nunito;
+  font-size: 20px;
+  text-align: center;
+  align-items: center;
+  font-weight: bold;
+  color: #000000;
+`
+const TextBorder = styled.div`
+{
+  position: absolute;
+  left: 45%;
+  width: 150px;
+  height: 5px;
+  background-color: #000000;
 }
 `
+const AlignAll = styled.div`
+{
+
+}
+`
+
 export default function About(props) {
   return (
     <Layout loggedIn={props.loggedIn} setLoggedIn={props.setLoggedIn}>
-      <TextContainer>
-        <h1>Who We Are</h1>
-        <br></br>
-        <h1>Waste Watchers is a global
-        recycling collective dedicated
-        to empowering humans
-        everywhere to actively engage
- in recycling.</h1>
-      </TextContainer>
+
+      <AlignAll>
+        <AboutTitle>
+          <h1>Who We Are</h1>
+        </AboutTitle>
+        <TextBorder />
+        <TextContainer>
+          Waste Watchers is a global
+          recycling collective dedicated
+          to empowering humans
+          everywhere to actively engage
+          in recycling.
+        </TextContainer>
+      </AlignAll>
     </Layout>
   )
 }
