2aeacb3572727761b83080089c77ed69b591afd2
zahrakhan <zahra.khan1@icloud.com>
2020-07-22 13:29:17 -0400

    add spinner container

--- client/src/components/shared/Loader/Loader.jsx
+++ client/src/components/shared/Loader/Loader.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
+
+const SpinnerContainer = styled.div`
+  border: 3px dashed black;
+  width: 300px;
+  height: 300px;
+`;
 
 const Spinner = styled.div`
-width: 75px;
-height: 75px;
-margin: 0;
-background: transparent;
-border-top: 4px solid blue;
-border-right: 4px solid transparent;
-border-radius: 50%;
-animation: 1s spin linear infinite;
-
-`
+  width: 75px;
+  height: 75px;
+  margin: 0;
+  background: transparent;
+  border-top: 4px solid blue;
+  border-right: 4px solid transparent;
+  border-radius: 50%;
+  animation: 1s spin linear infinite;
+`;
 
 export default function Loader() {
   return (
     <div>
-      <Spinner />
+      <SpinnerContainer>
+        <Spinner />
+      </SpinnerContainer>
     </div>
-  )
+  );
 }
